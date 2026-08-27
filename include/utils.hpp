fcc23b0bb3e79c2c8ed115969880fbfdd455b023
z-altair <altair993z@gmail.com>
2026-08-27 15:27:54 +0200

    Better vector2f implementation

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cmath>
 
 struct vector2i{
     int x = 0;
@@ -19,6 +20,10 @@ struct vector2f{
     vector2f operator-(const vector2f& other) const {
         return vector2f(x - other.x, y - other.y);
     }
+    // Cambio de signo
+    vector2f operator-() const {
+        return vector2f(-x, -y);
+    }
     // Multiplicacion por escalar
     vector2f operator*(float scalar) const {
         return vector2f(x * scalar, y * scalar);
@@ -28,11 +33,29 @@ struct vector2f{
     }
     // Division
     vector2f operator/(float scalar) const {
-        return vector2f(x / scalar, y / scalar);
+        return scalar == 0 ? vector2f(0,0) : vector2f(x / scalar, y / scalar);
     }
     // Producto escalar
     float dot(const vector2f& other) const {
         return x * other.x + y * other.y;
+    }
+    // Normalizar
+    vector2f normalize() const {
+        float length = sqrt(x * x + y * y);
+        if (length == 0.0f) return vector2f(0.0f, 0.0f);
+        return vector2f(x / length, y / length);
+    }
+    // Conseguir longitud
+    float magnitude() const {
+        return sqrt(x * x + y * y);
+    }
+
+    // Comparaciones logicas
+    bool operator==(const vector2f& other) const {
+        return x == other.x && y == other.y;
+    }
+    bool operator!=(const vector2f& other) const {
+        return !(*this == other);
     }
 };
 

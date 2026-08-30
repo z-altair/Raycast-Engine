184dfe4fb5ed6516cba2caf663bf01e2077bb8ec
z-altair <altair993z@gmail.com>
2026-08-30 12:11:14 +0200

    Engine: App bare skeleton

--- engine/include/app.hpp
+++ engine/include/app.hpp
@@ -2,9 +2,19 @@
 #include <raylib.h>
 
 class GameState{
-
+    public:
+    virtual void Enter() = 0;
+    virtual void Exit() = 0;
+    virtual void HandleEvent() = 0;
+    virtual void Update() = 0;
+    virtual void Draw() = 0;
 };
 
 class App{
-    
+    public:
+    App();
+    ~App();
+    GameState GetState();
+    void PushState();
+    GameState PopState();
 };
